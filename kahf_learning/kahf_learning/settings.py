f141a19fea95d6f7586f1bda664a7273e53e5569
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2026-04-08 13:26:14 +0530

    custom signup forms and logic
    for show custom fields in the allauth signup form,  add Userprofile model for manage users with customfields

--- kahf_learning/kahf_learning/settings.py
+++ kahf_learning/kahf_learning/settings.py
@@ -141,3 +141,5 @@ ACCOUNT_EMAIL_VERIFICATION = 'optional' # this is allauth specific
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 LOGIN_REDIRECT_URL = 'secret' #this is django specific
+
+ACCOUNT_SIGNUP_FORM_CLASS = 'accounts.forms.CustomSignupForm'
