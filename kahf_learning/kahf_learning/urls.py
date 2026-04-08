30a8b149c6cf5aae9b0838380ec13114e4a0a5d9
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2026-04-08 16:41:11 +0530

    django-allauth - Views and Template customization | Overriding django-allauth templates

--- kahf_learning/kahf_learning/urls.py
+++ kahf_learning/kahf_learning/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import CustomSignupView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/signup/", CustomSignupView.as_view(), name='account-signup'),
     path('accounts/', include('allauth.urls')),
     path("", include("accounts.urls")),
 ]
